3a5b9ba266f89b0b6265e4047b3c98ed355ebc0e
BrentynMorgan35 <54853239+BrentynMorgan35@users.noreply.github.com>
2021-01-08 10:58:07 -0600

    01/08/2021

--- CS_ASP_020/CS_ASP_020/MainWindow.xaml.cs
+++ CS_ASP_020/CS_ASP_020/MainWindow.xaml.cs
@@ -24,5 +24,39 @@ namespace CS_ASP_020
         {
             InitializeComponent();
         }
+        //print driver ticket
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tdriverLabel.Content = $"Driver - {driverTextBox}";
+
+            // convert text into numbers
+            double ss = double.Parse(socialTextBox.Text);
+            //print numbers
+            tSociallabel.Content = string.Format("SSN: {0:000-00-0000}", ss);
+
+            double pn = double.Parse(phoneTextBox.Text);
+
+            tPhoneLabel.Content = string.Format("Phone Numner {0:(000)000-0000}",pn);
+        
+            //Subtracts the miles
+            int miles = Convert.ToInt32(endMilesTextBox.Text) - Convert.ToInt32(startMilesTextBox.Text);
+            //print numbers
+            tMilesLabel.Content = $"Total Miles: {miles}";
+            //subtracts days to get days remaining 
+            int days = Convert.ToInt32((calanderReturn.SelectedDate.Value - calendarLeft.SelectedDate.Value).Days);
+
+            tDaysOutlebel.Content = $"Days Out:{days}";
+            //tells if the load is refridgerated and needs more pay
+            double pay = (fridgeCheck.IsChecked == true) ? miles * .37 : miles * .25;
+
+            tPayDueLabel.Content = string.Format("Pay Due: {0:c}", pay);
+            //get vacation
+            int vac = days / 5;
+
+            tVacationLabel.Content = $"Vacation Earned: {vac} Days";
+
+
+
+        }
     }
 }
