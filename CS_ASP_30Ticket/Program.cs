eb2eeae5f46bf4bdf7c752187472ddca7b1930f2
BrentynMorgan35 <54853239+BrentynMorgan35@users.noreply.github.com>
2021-02-10 11:04:03 -0600

    Add files via upload

---
+++ CS_ASP_30Ticket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CS_ASP_30Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentNum;
+            int speedLimit;
+            int ticketSpeed;
+            double price;
+
+
+        Console.Write("Enter Student Number: ");
+            studentNum = int.Parse(Console.ReadLine());
+        Console.Write("Eneter speed limit");
+            speedLimit = int.Parse(Console.ReadLine());
+        Console.Write("Enter speed of student");
+            ticketSpeed = int.Parse(Console.ReadLine());
+        Console.Write("Enter class of student by number shown- 0 Freshman, 1 Sophmore, 2 Jumior, 3 Senior");
+            switch (int.Parse(Console.ReadLine());
+            {
+                case 0:
+
+
+                default:
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
