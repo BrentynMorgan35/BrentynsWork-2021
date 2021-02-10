a8264c6b50d3d1fdeff9946ef0f4574e4676903c
BrentynMorgan35 <54853239+BrentynMorgan35@users.noreply.github.com>
2021-02-09 23:23:38 -0600

    Add files via upload

---
+++ CS_ASP_029Practice/CS_ASP_029Practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CS_ASP_029Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dayOftheweek;
+            string myDay;
+
+            Console.Write(" Please enter the day of the week (1 - Monday, 2 - Tuesday .... 5 - Friday) :");
+            dayOftheweek =int.Parse(Console.ReadLine());
+
+            switch (dayOftheweek)
+            {
+                case 1: myDay = "Monday";
+                    Console.WriteLine(myDay);
+                    break;
+                case 2: myDay = "Tuesday";
+                    Console.WriteLine(myDay);
+                    break;
+                case 3: myDay = "Wednesday";
+                    Console.WriteLine(myDay);
+                    break;
+                case 4: myDay = "Thursday";
+                    Console.WriteLine(myDay);
+                    break;
+                case 5: myDay = "Friday";
+                    Console.WriteLine(myDay);
+                    break;
+               
+      default:
+                    break;
+            }
+
+
+        }
+    }
+}
