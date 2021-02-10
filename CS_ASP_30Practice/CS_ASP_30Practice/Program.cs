557a9f55b807d18535500a708c9eafb224b43768
BrentynMorgan35 <54853239+BrentynMorgan35@users.noreply.github.com>
2021-02-09 22:56:03 -0600

    Add files via upload

---
+++ CS_ASP_30Practice/CS_ASP_30Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS_ASP_30Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inValue = 0;
+            string inString;
+
+            Console.WriteLine(" Enter your age :");
+            inString = Console.ReadLine();
+
+            while (int.TryParse(inString, out inValue)== false)
+            {
+                Console.WriteLine("Hey stupid enter an Interger");
+                Console.WriteLine("Please re-enter value :");
+                inString = Console.ReadLine();
+            }
+
+            Console.WriteLine("You enetered :" + inValue);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
